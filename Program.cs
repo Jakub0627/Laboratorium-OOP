317de37ad4348829784534928cc777cb9541d27a
MoneyHungryRat <kuba.gladysz@hotmail.com>
2023-10-25 13:20:45 +0200

    Praca na Lab 25.10

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using Lab1.Constructions;
+
+namespace Lab1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var Constr = new Construction(32, 32, 32, 16, "brik");
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine();
+        }
+    }
+}
