4d28e35d2abc3d239613752e9b6619321ba5a1bd
Jakub <kuba.gladysz@hotmail.com>
2023-11-18 17:55:19 +0100

    Lab2

--- LabOOP/Program.cs
+++ LabOOP/Program.cs
@@ -1,15 +1,30 @@
 ﻿using LabOOP.Constructions;
+using LabOOP.Constructions.Models;
 using System;
 
 namespace LabOOP
 {
-    internal class Program
+    class Program
     {
         static void Main(string[] args)
         {
-            var constr = new Construction(32, 23, 3, 6, "brik");
-            constr.BuildMaterial = "eee";
+            var constr = new Construction(32, 23, 3, 6, TypeOfMaterial.Brik);
+            var constr2 = new Construction();
+            var inputParam = new CreateConstructionDto()
+            {
+                Height = 23,
+                Width = 12,
+                Entrances = 1,
+                HumanCapacity = 1,
+                BuildMaterial = TypeOfMaterial.Brik
+            };
+
+            var constr3 = new Construction(inputParam);
             Console.WriteLine(constr.BuildMaterial);
+            Console.WriteLine(constr2.BuildMaterial);
+            Console.WriteLine(constr2.Width);
+            Console.WriteLine(constr2.Height);
+            Console.WriteLine(constr3.BuildMaterial);
         }
     }
 }
